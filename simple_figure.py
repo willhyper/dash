47595106ab314b7ede65d6d78b976f20d750a502
willhyper <willhyper@gmail.com>
2017-08-30 21:36:32 -0700

    simple figure

---
+++ simple_figure.py
@@ -0,0 +1,30 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import numpy as np
+
+x = np.array(range(100))
+y = np.sin(x/100)
+
+fig = {
+        'data': [   go.Scatter(x=x,y=y),
+                    go.Scatter(x=x,y=2*y)
+                ],
+        'layout' : go.Layout(
+            xaxis={'title': 't'},
+            yaxis={'title': 'magnitude'}
+        )
+}
+
+app = dash.Dash()
+
+app.layout = html.Div(
+    [
+        dcc.Graph(id='graph', figure=fig)
+    ]
+)
+
+if __name__ == '__main__':
+    app.run_server()
